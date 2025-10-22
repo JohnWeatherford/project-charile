62a080bed4b911fe0fd26fcc457e15909c8a7538
znasser46 <zacharynasser46@gmail.com>
2025-10-22 10:07:02 -0500

    Fix loop variable and log game data
    Updated the loop to iterate over 'game' instead of 'idea' and added a console log for each game.

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,8 @@ fetch('https://znasser46.github.io/project-charile/gameInfo.json').then(response
     .then(data => {
 
         const cards = document.getElementById('cardName');
-        data.games.forEach(idea => {
+        data.games.forEach(game => {
+            console.log(game);
             const card = document.createElement('div');
             cards.textContent = `${idea.name}, ${idea.description}`;
             .appendChild(cards);
