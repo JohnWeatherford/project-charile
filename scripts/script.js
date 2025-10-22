97df11258d02ed2ce159965fab0adc5e1cd52c63
znasser46 <zacharynasser46@gmail.com>
2025-10-22 10:44:29 -0500

    Add fetch and display functionality for game cards
    Fetch game information from a JSON file and display it as cards.

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+
+    
 fetch('https://znasser46.github.io/project-charile/gameInfo.json').then(response => response.json())
     .then(data => {
-
+        
         const cards = document.getElementById('cardName');
         data.games.forEach(game => {
             console.log(game);
             const card = document.createElement('div');
+            card.className = "cards";
+            
             card.textContent = `${game.name}, ${game.description}`;
             cards.appendChild(card);
         });
+       
     })
     .catch(error => console.error('Error fetching app ideas:', error));
 
