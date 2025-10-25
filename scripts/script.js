24298ed93af60a900f9544f0da343e51dea2d022
znasser46 <zacharynasser46@gmail.com>
2025-10-25 12:21:41 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -5,14 +5,41 @@ document.addEventListener("DOMContentLoaded", () => {
 fetch('https://znasser46.github.io/project-charile/gameInfo.json').then(response => response.json())
     .then(data => {
         
-        const cards = document.getElementById('cardName');
+        const cardset = document.getElementById('cardset');
         data.games.forEach(game => {
             console.log(game);
             const card = document.createElement('div');
             card.className = "cards";
+            const cardName = document.createElement('h1');
+            cardName.textContent = `${game.name}`;
+            card.appendChild(cardName);
+
+            const repoLink = document.createElement('a');
+            repoLink.className = "repoLinks";
+            repoLink.href = `${game.repo}`;
+            repoLink.textContent = "Repo Link";
+            repoLink.target = "_blank";
+            card.appendChild(repoLink);
+
+            const gameLink = document.createElement('a');
+            gameLink.className = "gameLinks";
+            gameLink.href = `${game.app}`;
+            gameLink.textContent = "Game Link";
+            gameLink.target = "_blank";
+            card.appendChild(gameLink);
+
+            const gameImg = document.createElement('img');
+            gameImg.className = "gameImg";
+            gameImg.src = `${game.img}`;
+            gameImg.alt = "game image";
+            card.appendChild(gameImg);
+
+
+            // card.textContent = ` ${game.repo}, ${game.app}, ${game.img}`;
             
-            card.textContent = `${game.name}, ${game.description}`;
-            cards.appendChild(card);
+            cardset.appendChild(card);
+
+            
         });
        
     })
